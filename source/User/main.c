5002bcb2c0588fea6091e9a90dca0313b7399b41
spmno <sunqingpeng@hotmail.com>
2014-04-29 22:55:51 +0800

    用标准库代码初始化。

--- source/User/main.c
+++ source/User/main.c
@@ -18,11 +18,11 @@ int main( void )
 	xQueueHandle xQueue;
 	
 	Stm32_Clock_Init();//系统时钟设置
-	RCC->APB2ENR |= 0x00000001; //开启afio时钟
-//	AFIO->MAPR = (0x00FFFFFF & AFIO->MAPR)|0x04000000;          //关闭JTAG 
-	RCC->APB2ENR|=0X0000001c;//先使能外设IO PORTa,b,c时钟
 	
 	TFT_Initialize();
+	Motor_GPIO_Init();
+	
+	
 	
 	xQueue = xQueueCreate( 10, sizeof( long ) );
 	if( xQueue != NULL )
@@ -31,14 +31,14 @@ int main( void )
 		xTaskCreate( Business_Entry, "Business_Entry", 1000, (void*)xQueue, 1, NULL );
 		xTaskCreate( Key_Entry, "Key_Entry", 1000, (void*)xQueue , 2 ,NULL );
 		xTaskCreate( HumiTemp_Entry, "HumiTemp_Entry", 1000, (void*)xQueue, 3, NULL );
-		xTaskCreate( Motor_Entry, "Motor_Entry", 1000, (void*)xQueue, 3, NULL );
+		//xTaskCreate( Motor_Entry, "Motor_Entry", 1000, (void*)xQueue, 3, NULL );
 	}
 
 	vTaskStartScheduler();
 	
 	while ( 1 )
 	{
-		// do something here
+
 	}
 	
 
